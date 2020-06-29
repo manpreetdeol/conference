5762200e40b4eabb46d3bffffedbe6d2fead077a
manpreetdeol <manpreetsingh.16.11.87@gmail.com>
2020-06-28 23:56:59 -0400

    Spring Java @Configuration - @Bean & Setter Injection

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,25 @@
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+//        return new SpeakerServiceImpl();
+
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        service.setRepository(getSpeakerRepository());
+
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
+}
---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    public List<Speaker> findAll(){
+        final List<Speaker> speakers = new ArrayList<Speaker>();
+
+        final Speaker speaker = new Speaker();
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+
+        speakers.add(speaker);
+
+        return speakers;
+    }
+}
